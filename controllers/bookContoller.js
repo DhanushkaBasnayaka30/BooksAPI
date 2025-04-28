55e16a3f83b3b2c819c85783610455753cd44338
Dhanushkabasnayaka99 <k9185.dhanuska@gmail.com>
2025-04-29 00:16:33 +0530

    3rd commit
    update router

--- controllers/bookContoller.js
+++ controllers/bookContoller.js
@@ -9,6 +9,7 @@ exports.getAllBooks = (req, res) => {
 	const endIndex = page * limit;
 
 	const pageinatedBooks = books.slice(startIndex, endIndex);
+	console.log("all books",pageinatedBooks);
 	res.json({
 		page,
 		limit,
@@ -22,8 +23,10 @@ exports.getBookById = (req, res) => {
 	const book = books.find((book) => book.id == parseInt(req.params.id));
 
 	if (!book) {
+		console.error()("Book not found");
 		return res.status(404).json({ error: "Book not Found" });
 	}
+	console.log("book-->",book);
 	res.json(book);
 };
 
@@ -37,19 +40,22 @@ exports.addBook = (req, res) => {
 	const { id, name, author, publishedYear } = req.body;
 	const existingBook = books.find((book) => book.id === id);
 	if (existingBook) {
+		console.log("Book with this ID already exists");
 		return res.status(400).json({ errors: "Book with this ID already exists" });
 	}
 
 	const newBook = { id, name, author, publishedYear };
 	books.push(newBook);
+	console.log("new book",newBook);
 	res.status(201).json(newBook);
 };
 
 //update book
 exports.updateBook = (req, res) => {
-	const book = books.find((book) => book.Id === parseInt(req.params.id));
+	const book = books.find((book) => book.id === parseInt(req.params.id));
 
 	if (!book) {
+		console.error('Book not found');
 		return res.status(404).json({ error: "Book not Found" });
 	}
 
@@ -57,19 +63,21 @@ exports.updateBook = (req, res) => {
 	if (name) book.name = name;
 	if (author) book.author = author;
 	if (publishedYear) book.publishedYear = publishedYear;
-
+	console.info("Book updated successfully");
 	res.json({ message: "Book updated successfully" });
 };
 
 //Delete Book
 exports.deleteBook = (req, res) => {
 	const bookIndex = books.findIndex(
-		(book) => book.Id === parseInt(req.params.id)
+		(book) => book.id === parseInt(req.params.id)
 	);
 
 	if (bookIndex === -1) {
+		console.error('Book Not Found');
 		return res.json(404).json({ error: "Book Not Found" });
 	}
 	books.splice(bookIndex, 1);
+	console.info("Book deleted successfully");
 	res.json({ message: "Book deleted successfully" });
 };
